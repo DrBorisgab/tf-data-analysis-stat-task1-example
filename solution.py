6dab489dc380ffab7ee16b80dcea761cdd59d474
DrBorisgab <43994457+DrBorisgab@users.noreply.github.com>
2023-04-06 11:28:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sts
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 530463349 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 67
+    e = sts.laplace().rvs(len(x))
+    a = (x - e)/t**2
+    return a.mean() # Ваш ответ
